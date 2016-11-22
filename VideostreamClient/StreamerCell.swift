fcd6bed6d154f2ce9db720023c3cb004d07e34b8
Randel Smith <homankids@gmail.com>
2016-11-22 09:16:02 -0600

    Added StreamerCell

---
+++ VideostreamClient/StreamerCell.swift
@@ -0,0 +1,14 @@
+//
+//  StreamerCell.swift
+//  VideostreamClient
+//
+//  Created by PC on 11/22/16.
+//  Copyright © 2016 Randel Smith rs@randelsmith.com. All rights reserved.
+//
+
+import UIKit
+
+class StreamerCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+}
